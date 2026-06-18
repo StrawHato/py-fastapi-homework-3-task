e0de1a5328a057b7880971254aea51b031a890fb
Maksym Pakula <gargantadoto@gmail.com>
2026-06-18 16:20:54 +0200

    add schemas for account endpoints

--- src/schemas/accounts.py
+++ src/schemas/accounts.py
@@ -1,6 +1,69 @@
-from pydantic import BaseModel, EmailStr, field_validator
+from pydantic import BaseModel, EmailStr, field_validator, ConfigDict
 
 from database import accounts_validators
 
 
-# Write your code here
+class UserRegistrationRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        try:
+            return accounts_validators.validate_password_strength(value)
+        except ValueError as e:
+            raise ValueError(str(e))
+
+
+class UserRegistrationResponseSchema(BaseModel):
+    id: int
+    email: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class UserActivationRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+
+
+class MessageResponseSchema(BaseModel):
+    pass
+
+
+class PasswordResetRequestSchema(BaseModel):
+    email: EmailStr
+
+
+class PasswordResetCompleteRequestSchema(BaseModel):
+    email: EmailStr
+    token: str
+    password: str
+
+    @field_validator("password")
+    @classmethod
+    def validate_password(cls, value: str) -> str:
+        try:
+            return accounts_validators.validate_password_strength(value)
+        except ValueError as e:
+            raise ValueError(str(e))
+
+
+class UserLoginResponseSchema(BaseModel):
+    access_token: str
+    refresh_token: str
+    token_type: str
+
+
+class UserLoginRequestSchema(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class TokenRefreshRequestSchema(BaseModel):
+    refresh_token: str
+
+
+class TokenRefreshResponseSchema(BaseModel):
+    access_token: str
